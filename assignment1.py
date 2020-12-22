e52e3132a09b40734fada9bcf5bfd4d9092c9330
Sherkhan Khanseiit <57889104+Sherhan187@users.noreply.github.com>
2020-12-22 23:45:33 +0600

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,41 @@
+
+#1
+name = input("Whay is you name?")
+print("My name is " + name)
+
+#2
+word = input("Enter any word:")
+print(len(word))
+
+#3
+a = int(input("Enter first num: "))
+b = int(input("Enter second num: "))
+print(a % b)
+
+#4
+import math
+a = int(input("Enter first num: "))
+b = int(input("Enter second num: "))
+c = int(input("Enter third num: "))
+print(math.sqrt(a*b*c))
+
+#5
+x = int(input())
+sqr = (x / 2) * (x / 2)
+func = 6.996 + 45 * sq + 2 * x
+print(func)
+
+#6
+import math
+
+a = str(input("Enter first str: "))
+b = str(input("Enter second str: "))
+c = str(input("Enter third str: "))
+x = len(a) * len(b) * len(c)
+print(math.sqrt(x))
+
+#7
+import math
+a = str(input("Enter any str: "))
+b = int(input("Enter any int: "))
+print(a, len(a), (len(a)) **b )
